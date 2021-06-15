5a7ccf6c2df4ac2613ce9a2ac8fb12d331b74f10
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-15 14:38:11 -0400

    class five

---
+++ app/js/scripts.js
@@ -0,0 +1,40 @@
+var tabs = document.querySelectorAll('nav a')
+contentPara = document.querySelector('.content')
+
+function makeActive(event) {
+  if (!event.target.matches('a')) return
+  makeInactive()
+  event.target.classList.add('active')
+}
+
+function makeInactive() {
+  tabs.forEach((tab) => tab.classList.remove('active'))
+}
+
+function setContentAccordingToHash() {
+  const type = window.location.hash.substring(1)
+  contentPara.innerHTML = data[type]
+}
+
+// function initializePage() {
+//   document.querySelector('nav a').classList.add('active')
+//   window.location.hash = 'cuisines'
+//   setContentAccordingToHash()
+// }
+
+function initializePage() {
+  if (!window.location.hash) {
+    window.location.hash = 'cuisines'
+    document.querySelector('[href="#cuisines"]').classList.add('active')
+  } else {
+    document
+      .querySelector(`[href="${window.location.hash}]"`)
+      .classList.add('active')
+  }
+  setContentAccordingToHash()
+}
+
+document.addEventListener('click', makeActive)
+window.addEventListener('hashchange', setContentAccordingToHash)
+
+initializePage()
